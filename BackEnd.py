a6c86951ca25e155c4ac6fa6bf679832b2e02cf1
abderrahimx05 <75130312+abderrahimx05@users.noreply.github.com>
2026-02-12 14:15:01 +0100

    update front

--- BackEnd.py
+++ BackEnd.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flasgger import Swagger
 import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+swagger = Swagger(app)
 
 def get_db():
     return mysql.connector.connect(
